73732190eb2d7c3954184ad88bf85eb122278725
bhanu gupta <bhanugupta.im@gmail.com>
2022-10-07 00:52:43 +0530

    initial commit

---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="shopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="Contact"),
+    path("tracker/", views.tracker, name="Tracker"),
+    path("search/", views.search, name="Search"),
+    path("productview/", views.productview, name="Productview"),
+    path("checkout/", views.checkout, name="Checkout"),
+]
---
+++ shop/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("we are at about page")
+    # return render(request, 'shop/index.html')
+
+def contact(request):
+    return HttpResponse("we are at contact page")
+    # return render(request, 'shop/index.html')
+
+def tracker(request):
+    return HttpResponse("we are tracker page")
+    # return render(request, 'shop/index.html')
+
+def search(request):
+    return HttpResponse("we are at search page")
+    # return render(request, 'shop/index.html')
+
+def productview(request):
+    return HttpResponse("we are at product view")
+    # return render(request, 'shop/index.html')
+
+def checkout(request):
+    return HttpResponse("we are at checkout page")
+    # return render(request, 'shop/index.html')
+
+
