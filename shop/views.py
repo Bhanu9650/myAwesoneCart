b876104752d8c454e4a482ddc5929bb16914a024
bhanu gupta <bhanugupta.im@gmail.com>
2022-10-09 00:00:48 +0530

    add products and divide by category

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    n = len(products)
+    nSlides = n//4 + ceil((n/2) - (n//4))
+
+    # params = {'no_of_slides': nSlides, 'range' : (1, nSlides), 'product': products}
+    allProds = [[products, range(3, nSlides), nSlides],
+              [products, range(3, nSlides), nSlides]]
+    params = {'allProds' : allProds} 
+    return render(request, 'shop/index.html', params)
 
 def about(request):
     # return HttpResponse("we are at about page")
